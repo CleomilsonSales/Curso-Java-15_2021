429a0fd100ce91b6f042de8aeac7d2cf9dcf8520
cleomilsonsales <cleomilsonsales@hotmail.com>
2020-11-23 22:47:09 -0300

    aula segunda antes de dumir

---
+++ exercicios/src/fundamentos/ConversaoTipoPrimitivosNumericos.java
@@ -0,0 +1,19 @@
+package fundamentos;
+
+public class ConversaoTipoPrimitivosNumericos {
+	
+	public static void main(String[] args) {
+		
+		//cast - conversão explicita
+		//basta colocar o tipo da variavel entre parênteses
+		
+		//cast é quando vc precisa converter de um primitivo maior
+		//para um primitivo menor - tipo de um double para um int
+		
+		float b = (float) 1.123456789116;
+		System.out.println(b);
+		
+		
+	}
+
+}
--- exercicios/src/fundamentos/NotacaoPonto.java
+++ exercicios/src/fundamentos/NotacaoPonto.java
@@ -2,6 +2,19 @@ package fundamentos;
 
 public class NotacaoPonto {
 	
+	/*
+	 tipo primitivos não tem notação de ponto pq não são objetos
+	 mas algunas primitivos basta chamar com a primeira letra maiuscula
+	 que ele sera um objeto e assim podera usar a notação ponto, isso 
+	 chama de wrapper.
+	 ex.:
+	 Integer - int
+	 Character - char
+	 
+	 os demais so iniciar com maiuscula que será um objeto
+	  */
+	
+	
 	public static void main(String[] args) {
 		String s = "Bom dia X";
 		s = s.replace("X", "Senhora");
