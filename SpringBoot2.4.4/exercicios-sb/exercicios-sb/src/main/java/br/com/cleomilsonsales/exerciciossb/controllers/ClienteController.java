c5e4af2a8a95f6f402111c0b87d9de7bac7448f4
Cleomilson Sales <cleomilsonsales@hotmail.com>
2021-04-06 21:50:30 -0300

    estudos de terça 06.04.21 21:50

--- SpringBoot2.4.4/exercicios-sb/exercicios-sb/src/main/java/br/com/cleomilsonsales/exerciciossb/controllers/ClienteController.java
+++ SpringBoot2.4.4/exercicios-sb/exercicios-sb/src/main/java/br/com/cleomilsonsales/exerciciossb/controllers/ClienteController.java
@@ -1,7 +1,9 @@
 package br.com.cleomilsonsales.exerciciossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cleomilsonsales.exerciciossb.models.Cliente;
@@ -14,4 +16,16 @@ public class ClienteController {
 	public Cliente obterCliente() {
 		return new Cliente(28,"Pedro", "123.456.789-10");
 	}
+	
+	@GetMapping("/{id}") //dessa forma fica variavel o valor passado.
+	public Cliente obterClientePorId(@PathVariable int id) {
+		return new Cliente(id, "Maria", "123.456.789-00");
+	}
+	
+	@GetMapping() 
+	public Cliente obterClientePorId2(
+			@RequestParam(name = "id", defaultValue = "1") int id) {
+		return new Cliente(id, "Maria Jose", "987.654.321-01");
+	}
+	
 }
