7a2425f2c68d9770a8b50b197ef17112733c555f
Cleomilson Sales <cleomilsonsales@hotmail.com>
2021-04-11 22:21:16 -0300

    estudos de domingo 11.04.21 22:21

--- SpringBoot2.4.4/exercicios-sb/exercicios-sb/src/main/java/br/com/cleomilsonsales/exerciciossb/controllers/ProdutoController.java
+++ SpringBoot2.4.4/exercicios-sb/exercicios-sb/src/main/java/br/com/cleomilsonsales/exerciciossb/controllers/ProdutoController.java
@@ -1,9 +1,17 @@
 package br.com.cleomilsonsales.exerciciossb.controllers;
 
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +21,73 @@ import br.com.cleomilsonsales.exerciciossb.models.repositories.ProdutoRepository
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutoController {
-	@Autowired //coloca automaticamente o objeto criado no repositorio
+	//@Transienti - caso a classe não persita no banco
+	@Autowired //coloca automaticamente o objeto criado no repositorio - injeção de dependencia --isso so funciona se o spring tiver criada a classe que esta injetando a dependencia
 	private ProdutoRepository produtoRepository; 
 	
-	@PostMapping
-	public @ResponseBody Produto novoProduto(@RequestParam String nome) {
-		Produto produto = new Produto(nome); 
+	/*@PostMapping
+	public @ResponseBody Produto novoProduto(
+			@RequestParam String nome,
+			@RequestParam double preco,
+			@RequestParam double desconto) {
+		Produto produto = new Produto(nome, preco, desconto); 
 		produtoRepository.save(produto);
 		return produto;
+	}*/
+	
+	/*
+	//forma separada de insert e update
+	@PostMapping
+	public @ResponseBody Produto novoProduto(@Valid Produto produto) {
+		//o sprint consegue instanciar um novo objeto apenas lendo os campos passados no post
+		produtoRepository.save(produto); //save - inserção ou edição por causa da interface
+		return produto;
 	}
+	
+	@PutMapping
+	public Produto alterarProduto(@Valid Produto produto) {
+		produtoRepository.save(produto);
+		return produto;
+	}*/
+	
+	//uma forma de generalizar o inserir e o update
+	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+	public @ResponseBody Produto salvarProduto(@Valid Produto produto) {
+		//o sprint consegue instanciar um novo objeto apenas lendo os campos passados no post
+		produtoRepository.save(produto); //save - inserção ou edição por causa da interface
+		return produto;
+	}
+	
+	@GetMapping
+	public Iterable<Produto> obterProdutos(){
+		return produtoRepository.findAll();
+	}
+	
+	@GetMapping(path = "/nome/{parteNome}")
+	public Iterable<Produto> obterProdutosPorNome(@PathVariable String parteNome){
+		//return produtoRepository.findByNomeContainingIgnoreCase(parteNome);
+		return produtoRepository.searchByNameLike(parteNome);
+	}
+	
+	@GetMapping(path = "/pagina/{numeroPagina}/{qtdPagina}")
+	public Iterable<Produto> obterProdutosPorPaginacao(
+			@PathVariable int numeroPagina,
+			@PathVariable int qtdPagina){
+		qtdPagina = qtdPagina >= 2 ? qtdPagina = 2 : qtdPagina; 
+		
+		Pageable page = PageRequest.of(numeroPagina, qtdPagina);
+		return produtoRepository.findAll(page);
+	}
+	
+	@GetMapping(path = "/{id}")
+	public Optional<Produto> obterProdutosPorId(@PathVariable int id){
+		return produtoRepository.findById(id);
+	}
+	
+	@DeleteMapping(path = "/{id}")
+	public void excluirProduto(@PathVariable int id) {
+		produtoRepository.deleteById(id);
+	}
+	
+	
 }
--- SpringBoot2.4.4/exercicios-sb/exercicios-sb/src/main/java/br/com/cleomilsonsales/exerciciossb/models/repositories/ProdutoRepository.java
+++ SpringBoot2.4.4/exercicios-sb/exercicios-sb/src/main/java/br/com/cleomilsonsales/exerciciossb/models/repositories/ProdutoRepository.java
@@ -1,13 +1,37 @@
 package br.com.cleomilsonsales.exerciciossb.models.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository; //tem tudo do crudRepository mais a opção de busca com paginação
+import org.springframework.data.repository.query.Param;
 
 import br.com.cleomilsonsales.exerciciossb.models.entities.Produto;
 
 public interface ProdutoRepository 
-	extends CrudRepository<Produto, Integer> {
+	//extends CrudRepository<Produto, Integer> { //forma simples do crud no spring
+	extends PagingAndSortingRepository<Produto, Integer> {
 	//responsavel pela persistencia
 	
 	
+	/*
+	 * algumas conversões para o spring implementar automaticamente:
+	 * palavras que contem
+	 * findByNomeContaining
+	 * findByNomeIsContaining
+	 * findByNomeContains
+	 * 
+	 * palavras que começa ou terminam
+	 * findByNomeStartsWith
+	 * findByNomeEndsWith
+	 * 
+	 * palavras que não cotem
+	 * findByNomeNotContaining*/
+	
+	
+	//metodo usando conversão, nesse caso não é necessário implentar, o spring irá fazer isso automaticamente
+	public Iterable<Produto> findByNomeContainingIgnoreCase(String parteNome);
+	
+	//outra forma de procurar palavras no banco, sendo mais especifica com o jpql
+	@Query("SELECT p FROM Produto p WHERE p.nome like %:nome%")
+	public Iterable<Produto> searchByNameLike(@Param("nome") String nome);
 	
 }
