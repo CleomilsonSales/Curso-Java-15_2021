293e25ed8b85c07a7c16b8015a90592f84eebebb
Cleomilson Sales <cleomilsonsales@hotmail.com>
2021-04-16 23:29:30 -0300

    estudos de sexta 16.04.21 23:29

---
+++ jpa2.2_hibernate5.4/src/teste/muitosparamuitos/NovoFilmeAtor.java
@@ -0,0 +1,23 @@
+package teste.muitosparamuitos;
+
+import infra.DAO;
+import modelo.muitospramuitos.Ator;
+import modelo.muitospramuitos.Filme;
+
+public class NovoFilmeAtor {
+	public static void main(String[] args) {
+		Filme filmeA = new Filme("Star Wars Ep. 4", 8.5);
+		Filme filmeB = new Filme("O Fugitivo", 8.9);
+		
+		Ator atorA = new Ator("Harrison Ford");
+		Ator atrizB = new Ator("Carrie Fisher");
+		
+		filmeA.adicionarAtor(atorA);
+		filmeA.adicionarAtor(atrizB);
+		filmeB.adicionarAtor(atorA);
+		
+		DAO<Filme> dao = new DAO<Filme>();
+		dao.incluirAtomico(filmeA);
+		
+	}
+}
